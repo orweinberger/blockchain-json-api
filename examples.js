8a59cd8226214eb13ce264548811773d78016fbb
orweinberger <orweinberger@gmail.com>
2013-11-04 15:51:30 +0200

    minor adjustment to the examples file

--- examples.js
+++ examples.js
@@ -10,6 +10,4 @@ bc.API('address', '1FfmbHfnpaZjKFvyi1okTjJJusN455paPH', function (res) {
   console.log(res);
 });
 
-bc.API('address', '1FfmbHfnpaZjKFvyi1okTjJJusN455paPH', function (res) {
-  console.log(res);
-});
+/* More methods are available on http://blockchain.info/api/blockchain_api */

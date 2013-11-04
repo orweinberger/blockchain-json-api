e94e10636d81cdb96dfedb84ef36e872e290cf36
orweinberger <orweinberger@gmail.com>
2013-11-04 17:39:41 +0200

    minor fixes to package.json

--- examples.js
+++ examples.js
@@ -1,13 +1,18 @@
 var blockchain = require('./lib/blockchain');
 
 var bc = new blockchain();
-         
+
 bc.API('tx-index', '12554260', function (res) {
-  console.log(res.time);
+  //console.log(res);
+});
+
+bc.API('block-height', '1000', function (res) {
+  //console.log(res);
 });
 
 bc.API('address', '1FfmbHfnpaZjKFvyi1okTjJJusN455paPH', function (res) {
   console.log(res);
 });
+
 
 /* More methods are available on http://blockchain.info/api/blockchain_api */

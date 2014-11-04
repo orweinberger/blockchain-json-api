33db0ad87cda4f0e11ecc684e09894154bfac95d
Mario Colque <colquemario@gmail.com>
2014-11-03 23:48:26 -0300

    improved examples syntax

--- examples.js
+++ examples.js
@@ -1,26 +1,19 @@
-var blockchain = require('./lib/blockchain');
-
-var bc = new blockchain();
+var blockchain = require('./lib/blockchain'),
+    bc = new blockchain();
 
 bc.API('tx-index', '1000', function (res, err) {
-  if (err)
-    console.log('Error! ', err)
-  else
-    console.log(res);  
+  if (err) return console.log('Error! ', err);
+  console.log(res);
 });
 
 bc.API('block-height', '1000', function (res, err) {
-  if (err)
-    console.log('Error! ', err)
-  else
-    console.log(res);
+  if (err) return console.log('Error! ', err);
+  console.log(res);
 });
 
 bc.API('address', '1LTNyzQDxyAU89BaQwk8vaePADnXYErYda', function (res, err) {
-  if (err)
-    console.log('Error! ', err)
-  else
-    console.log(res);
+  if (err) return console.log('Error! ', err);
+  console.log(res);
 });
 
 
